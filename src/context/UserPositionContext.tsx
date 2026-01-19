deb0c44ae05a522d1f216d8f2dc60de29958b522
ElsaDse <elsasifoua02@gmail.com>
2026-01-19 18:29:12 +0100

    Amélioration du design du site.

--- src/context/UserPositionContext.tsx
+++
@@ -1,5 +0,0 @@
-import type { Position } from '@/type';
-import { getUserPosition } from '@/utils';
-import { createContext } from 'react';
-
-export const UserPositionContext = createContext<Position>(await getUserPosition());
